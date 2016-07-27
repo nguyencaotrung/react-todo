60c6a97d04d868cb39f7b7e423b4a56899ca0bd5
Nguyen Cao Trung <trung.nguyen2@trginternational.com.vn>
2016-07-27 13:11:27 +0700

    Add filter functionality

--- app/test/api/TodoAPI.test.jsx
+++ app/test/api/TodoAPI.test.jsx
@@ -54,4 +54,51 @@ describe('TodoAPI',()=>{
 
 		});
 	});
+
+	describe('filterTodos',()=>{
+		var todos = [
+			{
+				id: 1,
+				text: 'Some text here',
+				completed: true
+			},
+			{
+				id: 2,
+				text: 'Other text here',
+				completed: false
+			},
+			{
+				id: 3,
+				text: 'Some text here',
+				completed: true
+			}
+		];
+
+		it('should return all items if showCompleted is true',()=>{
+			var filterTodos = TodoAPI.filteredTodos(todos, true, '');
+			expect(filterTodos.length).toBe(3);
+		});
+
+		it('should not return all items if showCompleted is false',()=>{
+			var filterTodos = TodoAPI.filteredTodos(todos, false, '');
+			expect(filterTodos.length).toBe(1);
+		});
+
+		it('should sort by completed status',()=>{
+			var filteredTodos = TodoAPI.filteredTodos(todos, true, '');
+			expect(filteredTodos[0].completed).toBe(false);
+		});
+
+		it('should filter todos by searchText',()=>{
+			var filterTodos = TodoAPI.filteredTodos(todos, true, 'some');
+			expect(filterTodos.length).toBe(2);
+		});
+
+		it('should return all todos if searchText is empty',()=>{
+			var filterTodos = TodoAPI.filteredTodos(todos, true, '');
+			expect(filterTodos.length).toBe(3);
+		});
+
+	});
+
 });
